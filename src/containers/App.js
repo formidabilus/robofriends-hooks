d470c0529f9eb17df749b2093f86388b688c378d
formidabilus <chiriac_razvan@ymail.com>
2020-11-18 15:23:52 +0200

    modified robofriends with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { HashRouter } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import ErrorBoundry from "../components/ErrorBoundry";
@@ -9,49 +9,41 @@ import "./App.css";
 
 const TITLE = "Robofriends | Razvan Chiriac";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1 className="tc">Loading...</h1>
-    ) : (
-      <div className="tc">
-        <Helmet>
-          <title> {TITLE} </title>
-        </Helmet>
-        <HashRouter basename={process.env.PUBLIC_URL}>
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundry>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundry>
-          </Scroll>
-        </HashRouter>
-      </div>
-    );
-  }
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  return !robots.length ? (
+    <h1 className="tc">Loading...</h1>
+  ) : (
+    <div className="tc">
+      <Helmet>
+        <title> {TITLE} </title>
+      </Helmet>
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
